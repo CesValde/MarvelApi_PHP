15890840b5653a096f2223bccdfaa4f2f3648904
CesValde <cesar.valderrama@est.fi.uncoma.edu.ar>
2024-04-23 23:04:15 -0300

    Creo archivos Head, Index2
    Usaremos para la app index2,

---
+++ funciones.php
@@ -0,0 +1,18 @@
+<?php 
+
+    function getData($url) {
+        $result = file_get_contents($url) ;
+        $data = json_decode($result, true) ;
+        return $data ;
+    }
+
+
+    function getMessageuntil($days) {
+        return match(true) {
+            $days == 0 => "Dia de cine 😎", 
+            $days == 1 => "Un dia para el estreno! ", 
+            $days < 7 => "Una semanaaa! ", 
+            $days < 30 => "Este mes vuelve deadpool 😈",
+            default => "$days dias hasta el estreno 🤬",
+        } ;
+    }
