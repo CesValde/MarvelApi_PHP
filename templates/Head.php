e0d71786d34edc2a9f621bf22039b31c3df3c969
CesValde <valderramacesar2002@gmail.com>
2024-04-25 00:33:23 -0300

    .
    .

--- templates/Head.php
+++ templates/Head.php
@@ -3,6 +3,6 @@
         <meta name="description" content="La proxima pelicula de marvel" >
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title> Cual es la proxima pelicula de marvel...? Es: <?= $title; ?></title>
-        <!-- pico css  -->
+        <!-- pico css-->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"/>
     </head>
