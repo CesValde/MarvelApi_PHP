1e712af7b93fe8a9426cf61711a80de45b65feaf
CesValde <valderramacesar2002@gmail.com>
2024-04-25 00:37:18 -0300

    header
    agg header

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     $nextMovieData = $nextMovie -> getData() ;
 ?>
 
-    <?php renderTemplate('Head', ['title' => $nextMovieData['title']]); ?> 
+    <?php renderTemplate('header', ['title' => $nextMovieData['title']]); ?> 
     <!-- Unimos el array $nextMovieData con el mensaje que retorna la funcion -->
     <?php renderTemplate("main", array_merge($nextMovieData, ["untilMessage" => $nextMovie -> getMessageUntil()])); ?>
     <?php renderTemplate("estilos"); ?>
