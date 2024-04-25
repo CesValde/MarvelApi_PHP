7c45213468c59c113d99128fdac1f27ab82bbd64
CesValde <valderramacesar2002@gmail.com>
2024-04-25 00:27:17 -0300

    Eliminamos index.php
    el index2 pasa a ser el index.php

--- index.php
+++ index.php
@@ -1,56 +1,19 @@
 <?php
 
-    // Declaracion de tipos en parametros, retornos dato que no sabia. 
-    // declare(strict_types=1) ;
-    // global $variable <-- usar una variable dentro de todos los ambitos
+    require_once "consts.php" ;
+    require_once "funciones.php" ;
+    require "clases/NextMovie.php" ;
 
-    const API_URL = "https://whenisthenextmcufilm.com/api" ;
-    # inicializar una nueva sesion de cURL -> ch = curl handle 
-    $ch = curl_init(API_URL) ;
-    // indicar que queremos recibir el resultado de la peticion y no mostrar en pantalla
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true) ;
-    /* Ejecutar la peticion y guardamos el resultado */
-    $result = curl_exec($ch) ;
-    $data = json_decode($result, true) ;
-    curl_close($ch) ;       // cerramos conexion de curl
-    
+    /*  Sacabamos los datos en variables ahora se saca con la clase NextMovie
+        $data = getData(API_URL) ;
+        $until_message = getMessageUntil($data["days_until"]) ; 
+    */
 
-    /* Una alternativa seria utilizar file_get_contents */
-    // $result = file_get_contents(API_URL) -> si solo quieres hacer un get de una api
-
+    $nextMovie = NextMovie::fetchAndCreateMovie(API_URL) ;
+    $nextMovieData = $nextMovie -> getData() ;
 ?>
 
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="estilos.css">
-        <title> Cual es la proxima pelicula de marvel ? </title>
-        <!-- pico css  -->
-        <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"/> -->
-    </head>
-
-    <main> 
-        <!-- <pre style="font-size: 12px; overflow: scroll; height: 300px">
-            <?php var_dump($data) ;  // mostramos los datos ?>
-        </pre> -->
-
-        <section> 
-            <img src="<?= $data["poster_url"] ; ?>" width="300" alt="Poster de <?= $data["title"] ; ?>"
-            style="border-radius: 16px" />
-        </section>
-
-        <hgroup>    
-            <h3> <?= $data["title"]; ?> Se estrena en <?= $data["days_until"] ; ?> dias </h3>
-            <p> Fecha de estreno: <?= $data["release_date"]; ?> </p>
-            <p> La siguiente es: <?= $data["following_production"]["title"]; ?> </p>
-        </hgroup>
-    </main> 
-
-
-
-    <body>
-        <h1> PROXIMO ESTRENO </h1>
-    </body>
-    </html>
+    <?php renderTemplate('head', ['title' => $nextMovieData['title']]); ?> 
+    <!-- Unimos el array $nextMovieData con el mensaje que retorna la funcion -->
+    <?php renderTemplate("main", array_merge($nextMovieData, ["untilMessage" => $nextMovie -> getMessageUntil()])); ?>
+    <?php renderTemplate("estilos"); ?>
